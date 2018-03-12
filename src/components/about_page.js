b80e8d94d8bd4b56d40be85318c4ac3d993333da
Sh4reef <30282584+Sh4reef@users.noreply.github.com>
2018-03-12 20:23:00 +0300

    update some pages

--- src/components/about_page.js
+++ src/components/about_page.js
@@ -3,7 +3,7 @@ var React = require('react');
 function About(props) {
   var techologies = ['React', 'React Router', 'Flux', 'Node'];
   return (
-    <div className="container">
+    <div className="container my-5">
       <h4>This application uses these technologies:</h4>
       <ul>
         {
--- src/components/home_page.js
+++ src/components/home_page.js
@@ -1,33 +1,18 @@
 var React = require('react');
 var createReactClass = require('create-react-class');
+var Link = require('react-router-dom').Link;
 
 function Home(props) {
   return React.createElement('div', {className: 'jumbotron'},
     React.createElement('h1', {className: 'display-4'},
-    'Hello, world!'),
+    'Pluralsight Administration'),
     React.createElement('p', {className: 'lead'}, 
     `
-    This is a simple hero unit,
-    a simple jumbotron-style 
-    component for calling extra 
-    attention to featured content or 
-    information.
+    React, React Router, And Flux for ultra-responsive web apps.
     `),
     React.createElement('hr', {className: 'my-4'}),
-    React.createElement('p', null, 
-    `It uses utility classes 
-    for typography and spacing 
-    to space content out within 
-    the larger container.`),
     React.createElement('p', {className: 'lead'},
-    React.createElement(
-    'a', 
-    {
-      className: 'btn btn-primary btn-lg',
-      href: '#',
-      role: 'button'
-    }, 
-    'Learn more'))
+    (<Link className="btn btn-primary" to="/about">Learn more</Link>))
   )
 }
 
